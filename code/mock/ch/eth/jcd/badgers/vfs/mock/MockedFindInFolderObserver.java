202aeb6f92b6c9f8141b40b75c811831b635aef5
Philipp Rohr <rohrp@student.ethz.ch>
2013-04-04 19:34:19 +0000

    added a new method on FindInFolderObserver

--- code/mock/ch/eth/jcd/badgers/vfs/mock/MockedFindInFolderObserver.java
+++ code/mock/ch/eth/jcd/badgers/vfs/mock/MockedFindInFolderObserver.java
@@ -1,13 +1,32 @@
 package ch.eth.jcd.badgers.vfs.mock;
 
+import org.apache.log4j.Logger;
+
 import ch.eth.jcd.badgers.vfs.core.interfaces.FindInFolderObserver;
 import ch.eth.jcd.badgers.vfs.core.interfaces.VFSPath;
+import ch.eth.jcd.badgers.vfs.exception.VFSException;
 
 public class MockedFindInFolderObserver implements FindInFolderObserver {
+	private static final Logger LOGGER = Logger.getLogger(MockedFindInFolderObserver.class);
 
 	@Override
-	public void foundEntry(VFSPath entry) {
-		System.out.println("Entry found: " + entry.toString());
+	public void foundEntry(VFSPath path) {
+		try {
+			LOGGER.info("Entry found: " + path.getAbsolutePath());
+		} catch (VFSException e) {
+			LOGGER.error("Error while getting absolute path", e);
+		}
+	}
+
+	@Override
+	public boolean stopSearch(VFSPath currentDirectory) {
+		try {
+			LOGGER.info("current search dir" + currentDirectory.getAbsolutePath());
+		} catch (VFSException e) {
+			LOGGER.error("Error while getting absolute path", e);
+		}
+		// shall not stop the search
+		return false;
 	}
 
 }
--- code/src/ch/eth/jcd/badgers/vfs/core/interfaces/FindInFolderObserver.java
+++ code/src/ch/eth/jcd/badgers/vfs/core/interfaces/FindInFolderObserver.java
@@ -15,4 +15,16 @@ public interface FindInFolderObserver {
 	 *            entry which matches
 	 */
 	void foundEntry(VFSPath path);
+
+	/**
+	 * This method informs the "find"-implementation, whether the find shall be stopped. This method could for instance return false, if on the GUI a
+	 * "STOP-Search" button was clicked. To inform the Observer on which directory the search is in exactly this moment the "find"-implementation should pass
+	 * this directory via the currentDirectory parameter.
+	 * 
+	 * The "find"-implementation should check this method several times during search.
+	 * 
+	 * @param currentDirectory
+	 * @return true if this search shall be stopped
+	 */
+	boolean stopSearch(VFSPath currentDirectory);
 }
