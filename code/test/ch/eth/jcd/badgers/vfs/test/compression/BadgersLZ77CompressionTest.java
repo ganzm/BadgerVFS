36b70fb9867a90e1ec0394d2e40dd66e6d70710a
Matthias Ganz <ganzm@student.ethz.ch>
2013-04-08 21:51:51 +0000

    reuse some code

--- code/test/ch/eth/jcd/badgers/vfs/test/compression/BadgersLZ77CompressionTest.java
+++ code/test/ch/eth/jcd/badgers/vfs/test/compression/BadgersLZ77CompressionTest.java
@@ -24,40 +24,29 @@ public class BadgersLZ77CompressionTest {
 
 	@Test
 	public void testMaximumCompression() throws IOException {
-
 		byte[] rawData = new byte[2048];
-		byte[] rawDataCopy = rawData.clone();
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-		BadgersLZ77CompressionOutputStream out = new BadgersLZ77CompressionOutputStream(outputStream);
-		out.write(rawData);
-		out.flush();
-		out.close();
-
-		byte[] encrypted = outputStream.toByteArray();
-		LOGGER.debug("Byte length before encryption: " + rawData.length);
-		LOGGER.debug("Byte length after encryption:  " + encrypted.length);
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(encrypted);
-
-		BadgersLZ77CompressionInputStream in = new BadgersLZ77CompressionInputStream(inputStream);
-		Assert.assertEquals(rawData.length, in.read(rawDataCopy));
-		in.close();
-
-		for (int i = 0; i < rawData.length; i++) {
-			Assert.assertEquals(rawData[i], rawDataCopy[i]);
-		}
-
+		testCompress(rawData);
 	}
 
 	@Test
 	public void testRandomInput() throws IOException {
-
 		Random rnd = new Random();
 		byte[] rawData = new byte[rnd.nextInt(100000)];
-		// byte[] rawData = new byte[] { -1, -1, -2 };
-		byte[] rawDataCopy = new byte[rawData.length];// = new byte[2048];
 		rnd.nextBytes(rawData);
+
+		testCompress(rawData);
+	}
+
+	@Test
+	public void testStringInput() throws IOException {
+		String stringData = "Miss Kalissippi from Mississippi is a " + "cowgirl who yells yippi when she rides her horse in "
+				+ "the horse show in Mississippi.";
+
+		testCompress(stringData.getBytes());
+	}
+
+	private void testCompress(byte[] rawData) throws IOException {
+		byte[] rawDataCopy = new byte[rawData.length];
 
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
@@ -73,42 +62,14 @@ public class BadgersLZ77CompressionTest {
 
 		BadgersLZ77CompressionInputStream in = new BadgersLZ77CompressionInputStream(inputStream);
 		in.read(rawDataCopy);
+
+		Assert.assertEquals(-1, in.read());
+
 		in.close();
 		Assert.assertEquals(rawData.length, rawDataCopy.length);
 
 		for (int i = 0; i < rawData.length; i++) {
 			Assert.assertEquals("Expected equal data, is not equal at " + i, rawData[i], rawDataCopy[i]);
 		}
-
-	}
-
-	@Test
-	public void testStringInput() throws IOException {
-
-		String stringDatas = "Miss Kalissippi from Mississippi is a " + "cowgirl who yells yippi when she rides her horse in "
-				+ "the horse show in Mississippi.";
-		byte[] rawDataCopy = new byte[2048];
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-		BadgersLZ77CompressionOutputStream out = new BadgersLZ77CompressionOutputStream(outputStream);
-		out.write(stringDatas.getBytes());
-		out.flush();
-		out.close();
-
-		byte[] encrypted = outputStream.toByteArray();
-		LOGGER.debug("Byte length before encryption: " + stringDatas.length());
-		LOGGER.debug("Byte length after encryption:  " + encrypted.length);
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(encrypted);
-
-		BadgersLZ77CompressionInputStream in = new BadgersLZ77CompressionInputStream(inputStream);
-		in.close();
-		Assert.assertEquals(stringDatas.length(), in.read(rawDataCopy));
-
-		byte[] rawData = stringDatas.getBytes();
-		for (int i = 0; i < rawData.length; i++) {
-			Assert.assertEquals("Expected equal data, is not equal at " + i, rawData[i], rawDataCopy[i]);
-		}
-
 	}
 }
