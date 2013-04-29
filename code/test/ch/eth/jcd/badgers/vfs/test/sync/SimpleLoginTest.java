40f747c29ddb24ab41ee8cf57f41ad042460dd03
Matthias Ganz <ganzm@student.ethz.ch>
2013-04-29 06:45:35 +0000

    fixed unittest

--- code/test/ch/eth/jcd/badgers/vfs/test/sync/SimpleLoginTest.java
+++ code/test/ch/eth/jcd/badgers/vfs/test/sync/SimpleLoginTest.java
@@ -3,8 +3,8 @@ package ch.eth.jcd.badgers.vfs.test.sync;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,6 +14,7 @@ import ch.eth.jcd.badgers.vfs.sync.client.ConnectionStatus;
 import ch.eth.jcd.badgers.vfs.sync.client.RemoteManager;
 import ch.eth.jcd.badgers.vfs.sync.server.ServerConfiguration;
 import ch.eth.jcd.badgers.vfs.sync.server.SynchronisationServer;
+import ch.eth.jcd.badgers.vfs.sync.server.UserAccount;
 import ch.eth.jcd.badgers.vfs.test.testutil.UnittestLogger;
 
 public class SimpleLoginTest implements ConnectionStateListener {
@@ -23,49 +24,58 @@ public class SimpleLoginTest implements ConnectionStateListener {
 	private static RemoteManager clientRemoteManager;
 	private static String hostLink = "localhost";
 
-	private ConnectionStatus status;
+	private static final String username = "asdf";
+	private static final String password = "asdf";
 
-	private final String username = "asdf";
-	private final String password = "asdf";
+	private ConnectionStatus status = ConnectionStatus.DISCONNECTED;
 
 	@BeforeClass
-	public static void beforeClass() throws VFSException {
+	public static void beforeClass() {
 		UnittestLogger.init();
+	}
 
+	@Before
+	public void before() throws VFSException {
 		setupSynchronisationServer();
-
 		setupClient();
 	}
 
-	@AfterClass
-	public static void afterClass() throws VFSException {
+	public void after() throws VFSException {
 		tearDownClient();
 		tearDownSynchronisationServer();
 	}
 
 	@Test
 	public void testLogin() throws InterruptedException {
+
+		long t = System.currentTimeMillis();
+		long timeout = 5000;
+		while (status != ConnectionStatus.CONNECTED && (System.currentTimeMillis() - t) < timeout) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ex) {
+			}
+		}
+
 		final CountDownLatch lock = new CountDownLatch(1);
-
 		final ConnectionStatus[] statusResult = new ConnectionStatus[1];
-		clientRemoteManager.startLogin(username, password, new ConnectionStateListener() {
-
+		boolean result = clientRemoteManager.startLogin(username, password, new ConnectionStateListener() {
 			@Override
 			public void connectionStateChanged(final ConnectionStatus status) {
-
 				statusResult[0] = status;
 				lock.countDown();
-
 			}
 		});
+
+		Assert.assertTrue(clientRemoteManager.getConnectionStatus() + "", result);
 		lock.await(5000, TimeUnit.MILLISECONDS);
 
 		Assert.assertEquals(ConnectionStatus.LOGGED_IN, statusResult[0]);
-
 	}
 
-	private static void setupClient() {
+	private void setupClient() {
 		clientRemoteManager = new RemoteManager(hostLink);
+		clientRemoteManager.addConnectionStateListener(this);
 		clientRemoteManager.start();
 
 	}
@@ -82,6 +92,11 @@ public class SimpleLoginTest implements ConnectionStateListener {
 
 	private static ServerConfiguration createServerConfiguration() throws VFSException {
 		final ServerConfiguration config = new ServerConfiguration("");
+
+		if (!config.accountExists(username)) {
+			UserAccount userAccount = new UserAccount(username, password);
+			config.setUserAccount(userAccount);
+		}
 		return config;
 	}
 
@@ -90,7 +105,8 @@ public class SimpleLoginTest implements ConnectionStateListener {
 	}
 
 	@Override
-	public void connectionStateChanged(final ConnectionStatus status) {
+	public void connectionStateChanged(ConnectionStatus status) {
 		this.status = status;
 	}
+
 }
